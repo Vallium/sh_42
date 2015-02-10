6054ee963b098493a7b94203ff16e7bb3633d0a8
Vallium <aalliot@student.42.fr>
2015-02-10 21:33:19 +0100

    tests

--- src/main.c
+++ src/main.c
@@ -11,9 +11,39 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
+#include "libft.h"
+#include "get_next_line.h"
 
-int		main(void)
+//#define BUFF_SIZE 1024
+
+int		main(int argc, char *argv[])
 {
-	printf("Minishell_1\n");
+	char	*args;
+	char	**ex;
+	int		ret;
+	pid_t	father;
+
+
+	(void)argc;
+	(void)argv;
+	args = NULL;
+	while(42)
+	{
+		father = fork();
+		if (father > 0)
+		{
+			waitpid(father, NULL, 0);
+		}
+		if (father == 0)
+		{
+			ft_putstr("$> ");
+			while ((ret = get_next_line(0, &args)))
+			{
+				ex = ft_strsplit(args, ' ');
+				execve("/bin/ls", ex, NULL);
+			}
+		}
+	}
 	return(0);
 }
