234db9ea09bd737db0656be42e7f9c53997b457b
Spectre <antoine@doussaud.org>
2015-02-20 20:05:35 +0100

    asdasd

--- src/main.c
+++ src/main.c
@@ -54,7 +54,11 @@ int		main(int argc, char *argv[], char *envp[])
 	while (42)
 	{
 		prompt(env);
-		get_next_line(0, &line);
+		if (get_next_line(0, &line) < 1)
+		{
+			ft_putchar('\n');
+			ft_exit();
+		}
 		command(line, &env);
 	}
 	return (0);
