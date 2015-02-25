332ddaf6dbacff520aa614cad3bc0a38afb91d22
Vallium <aalliot@student.42.fr>
2015-02-25 11:19:18 +0100

    strclean

--- libft/src/ft_strclean.c
+++ libft/src/ft_strclean.c
@@ -8,6 +8,86 @@ static unsigned int		ft_sp(char c)
 
 #include <stdio.h>
 
+void				ft_bzero(void *s, size_t n)
+{
+	unsigned char	*str;
+
+	str = (unsigned char*)s;
+	while (n--)
+		*str++ = 0;
+}
+
+char			*ft_strnew(size_t size)
+{
+	char		*str;
+
+	str = (char*)malloc(sizeof(char) * (size));
+	if (!str)
+		return (NULL);
+	ft_bzero(str, size);
+	return (str);
+}
+
+size_t			ft_strlen(const char *s)
+{
+	size_t		i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char			*ft_strsub(const char *s, unsigned int start, size_t len)
+{
+	char		*dst;
+	char		*ret;
+
+	if (!len)
+		return ("");
+	if (!s || !*s)
+		return (NULL);
+	dst = (char*)malloc(sizeof(char) * (len + 1));
+	if (!dst)
+		return (NULL);
+	ret = dst;
+	while (len--)
+		*dst++ = s[start++];
+	*dst = 0;
+	return (ret);
+}
+
+
+char					*ft_strtrim(char *s)
+{
+	char				*dst;
+	char				*tmp;
+	int					i;
+	int					e;
+
+	tmp = s;
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (ft_sp(*s++))
+		i++;
+	s -= i + 1;
+	e = ft_strlen(s) - 1;
+	if (!*s || e == i - 1)
+	{
+		free(tmp);
+		return (dst = ft_strnew(1));
+	}
+	if (i != (e + 1))
+		while (ft_sp(s[e]))
+			e--;
+	dst = ft_strsub(s, i, (e - i + 1));
+	free(tmp);
+	return (dst);
+}
+
 char					*ft_strclean(char *str)
 {
 	char	*tmp;
@@ -52,7 +132,7 @@ char					*ft_strclean(char *str)
 			tmp++;
 	}
 	*tmp_ret = 0;
-
+	ret = ft_strtrim(ret);
 	printf("%s\n", ret);
 	return (ret);
 }
