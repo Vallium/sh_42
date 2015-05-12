2323d5504e61b9a6ce6cf70f19259a0854164fa0
Vallium <aalliot@student.42.fr>
2015-05-12 18:45:34 +0200

    le parseur il pik il pik

--- src/parse.c
+++ src/parse.c
@@ -10,11 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "sh_1.h"
+//#include "sh_1.h"
+
+#include <string.h>
 
 static int		is_tab(char c)
 {
 	return (c == ' ' || c == '\t');
+}
+
+char			is_cmd(char str)
+{
+	return (str == '|');
 }
 
 static int		word_len(char *str)
@@ -22,6 +29,9 @@ static int		word_len(char *str)
 	int		i;
 
 	i = -1;
+
+	/*if is_cmd(str)
+		return (-2);*/
 	while (*str && !is_tab(*str))
 	{
 		if (*str == '\"')
@@ -63,8 +73,16 @@ static int			word_count(char *str)
 					i++;
 				}
 			}
+			else if (is_cmd(*str))
+			{
+				nb++;
+				str++;
+				break;
+			}
 			else
+			{
 				i++;
+			}
 			str++;
 		}
 		if (i > -1)
@@ -75,12 +93,14 @@ static int			word_count(char *str)
 	return nb;
 }
 
-static char			*word_malloc(char **str)
+static char			*word_malloc(char **str, int *cmd)
 {
 	int		i;
 	int		j;
 	char	*ret;
 	char	*tmp;
+
+	*cmd = 0;
 
 	i = 0;
 	j = 0;
@@ -95,6 +115,11 @@ static char			*word_malloc(char **str)
 				(*str)++;
 				i++;
 			}
+		}
+		else if (is_cmd(**str))
+		{
+			*cmd = '|';
+			break;
 		}
 		else
 			i++;
@@ -116,27 +141,58 @@ static char			*word_malloc(char **str)
 }
 
 
-char			**command_line_parser(char *line)
+typedef struct s_test
 {
-	char	**ret;
-	char	**tmp;
+	char	type;
+	char	*data;
+}			t_test;
+
+t_test			*command_line_parser(char *line)
+{
+	t_test		*ret;
+	t_test		*tmp;
 	char	*tmp_line;
 	int		nb;
+	int 	cmd;
 
 	tmp_line = line;
 	nb = word_count(line);
-	if (!(ret = (char**)malloc(sizeof(char*) * (nb + 1))))
+
+	printf("nb : %d\n",nb);
+
+	if (!(ret = (t_test*)malloc(sizeof(t_test) * (nb))))
 		return (NULL);
 	tmp = ret;
 	while (*tmp_line)
 	{
 		if (word_len(tmp_line) > -1)
 		{
-			*tmp = word_malloc(&tmp_line);
+			tmp->type = 'w';
+			tmp->data = word_malloc(&tmp_line, &cmd);
+			if (cmd == '|')
+			{
+				tmp++;
+				tmp->type = '|';
+				tmp->data = 0;
+			}
 			tmp++;
 		}
 		tmp_line++;
 	}
-	*tmp = 0;
 	return (ret);
 }
+
+ int		main() {
+ 	char	*line = " cat  	\"salut les gars\"  \"\" et  cat\"|\"grep puis \"alors quoi\"sdad \" hello \" hein  \"sdfsdf\"  ";
+
+ 	t_test *tab = command_line_parser(line);
+
+	int i = 0;
+
+	for (;i < 10;i++)
+	{
+		printf("%c : %s\n", tab[i].type, tab[i].data);
+	}
+
+ 	return 0;
+ }
