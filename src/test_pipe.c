e7669d86bc6f64e1d831548e25fd4dbfc2b9dc61
Vallium <aalliot@student.42.fr>
2015-07-04 17:03:30 +0200

    multi pipe test OK

--- src/test_pipe.c
+++ src/test_pipe.c
@@ -2,70 +2,87 @@
 #include <string.h>
 #include <stdio.h>
 #include <sys/wait.h>
-# include "libft.h"
+// # include "libft.h"
 
 void		run_pipe (void) {
 	int const	READ_END = 0;
 	int const	WRITE_END = 1;
+	int			fd_in = 0;
 	pid_t		child = -1;
 	int			pdes[2];
-
 	char	*cmd1 = NULL;
 	char	*cmd2 = NULL;
+	char	*cmd3 = NULL;
 	char	*args1[3];
 	char	*args2[3];
+	char	*args3[3];
 
-	cmd1 = "/bin/cat";
+	cmd1 = "/bin/ls";
 	args1[0] = cmd1;
-	args1[1] = "-e";
+	args1[1] = "-l";
 	args1[2] = NULL;
 
 	cmd2 = "/usr/bin/grep";
 	args2[0] = cmd2;
-	args2[1] = "test";
+	args2[1] = "test_pipe.c";
 	args2[2] = NULL;
 
-	pipe(pdes);		// create pipe
+	cmd3 = "/bin/cat";
+	args3[0] = cmd3;
+	args3[1] = "-e";
+	args3[2] = NULL;
 
-	child = fork();
+	char **tab[4] = {args1, args2, args3, NULL};
+	char ***ptr = tab;
+	while (*ptr) {
+		pipe(pdes);		// create pipe
 
-	switch ((int)child) {
-		case -1:										// if error
-			close(pdes[READ_END]);
-			close(pdes[WRITE_END]);
-			perror("error");
-		case 0:											// if cmd1
-			printf("run %s\n", cmd1);
-			dup2(pdes[WRITE_END], STDOUT_FILENO);
-			close(pdes[READ_END]);
-			ft_putstr_fd("hello\n", pdes[WRITE_END]);
-			execve(cmd1, args1, NULL);
-			perror("error");
-			break;
-		default:										// if cmd2
-			printf("run %s\n", cmd2);
-			dup2(pdes[READ_END], STDIN_FILENO);
-			close(pdes[WRITE_END]);
-			execve(cmd2, args2, NULL);
-			break;
+		child = fork();
+
+		switch ((int)child) {
+			case -1:										// if error
+				close(pdes[0]);
+				close(pdes[1]);
+				perror("error");
+			case 0:											// if cmd1
+				// printf("run %s\n", cmd1);
+				dup2(fd_in, 0);
+				if (*(ptr + 1))
+					dup2(pdes[1], 1);
+				close(pdes[0]);
+				// ft_putstr_fd("hello\n", pdes[WRITE_END]);
+				execve(*ptr[0], *ptr, NULL);
+				perror("error");
+				break;
+			default:										// if cmd2
+				// printf("run %s\n", cmd2);
+				wait(NULL);
+				// dup2(pdes[READ_END], STDIN_FILENO);
+				close(pdes[1]);
+				// execve(cmd2, args2, NULL);
+				fd_in = pdes[0];
+				ptr++;
+				break;
+		}
+
 	}
 }
 
 int			main(void) {
-	pid_t	child = -1;
-
-	child = fork();
-
-	switch ((int)child) {
-		case -1:
-			perror("error");
-			break;
-		case 0:
-			run_pipe();
-			perror("error");
-			break;
-	}
-	wait(NULL);
-
+	// pid_t	child = -1;
+	//
+	// child = fork();
+	//
+	// switch ((int)child) {
+	// 	case -1:
+	// 		perror("error");
+	// 		break;
+	// 	case 0:
+	// 		run_pipe();
+	// 		perror("error");
+	// 		break;
+	// }
+	// wait(NULL);
+	run_pipe();
 	return 0;
 }
